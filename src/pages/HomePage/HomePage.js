c5009f3c570c6a4c1950f053fe6602c3dd147f4c
neda <neda.shadkam2003@gmail.com>
2024-06-02 17:36:15 +0330

    HomePage changed

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -11,9 +11,23 @@ const HomePage = () => {
             <main className="homepage-container">
                 <Carousel />
                 <Suggested />
+                <div className="main-buttom">
+                    <div>
+                        <img src="https://sofaland.ir/wp-content/uploads/2019/07/22414818.svg" alt="" />
+                        <span>پرداخت آنلاین</span>
+                    </div>
+                    <div>
+                        <img src="https://sofaland.ir/wp-content/uploads/2019/07/a9286d2f.svg" alt="" />
+                        <span>پشتیبانی ۲۴ ساعته</span>
+                    </div>
+                    <div>
+                        <img src="https://sofaland.ir/wp-content/uploads/2019/07/8f570b58.svg" alt="" />
+                        <span>تحویل ایمن</span>
+                    </div>
+                </div>
             </main>
             <Footer />
-            <BackToTop/>
+            <BackToTop />
         </>
     )
 }
