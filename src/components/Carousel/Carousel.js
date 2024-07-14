9df6455616497e2ce68e7066d28d89ecec10d1b5
neda <neda.shadkam2003@gmail.com>
2024-07-14 12:39:02 +0330

    carousel changed

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Carousel.css';
 import image1 from '../../assets/images/homepage-carousel/carousel-img-1.jpg';
 import image2 from '../../assets/images/homepage-carousel/carousel-img-2.jpg';
@@ -6,56 +6,49 @@ import image3 from '../../assets/images/homepage-carousel/carousel-img-3.jpg';
 import image4 from '../../assets/images/homepage-carousel/carousel-img-4.jpeg';
 import image5 from '../../assets/images/homepage-carousel/carousel-img-5.jpg';
 import image6 from '../../assets/images/homepage-carousel/carousel-img-6.jpg';
+import WriteText from '../WriteText';
 
 const Carousel = () => {
+    const backgroundImagesArray = [image1, image2, image3, image4, image5, image6];
+    const imageCount = backgroundImagesArray?.length;
     const [currentIndex, setCurrentIndex] = useState(0);
     const [img, setImage] = useState(getImageByIndex(0));
-    const [index, setIndex] = useState(0)
-    const [letters, setLetters] = useState('');
+
+    useEffect(() => {
+        const imageInterval = setInterval(() => increase(), 5000);
+        return () => {
+            clearInterval(imageInterval);
+        }
+    });
 
     function getImageByIndex(index) {
-        const backgroundImagesArray = [image1, image2, image3, image4, image5, image6];
-        if (index <= backgroundImagesArray?.length) {
+        if (index < imageCount) {
             return backgroundImagesArray[index];
         }
+        return backgroundImagesArray[0];
     }
 
     function increase() {
-        if (currentIndex < 5) {
-            setCurrentIndex(currentIndex + 1);
-            setImage(getImageByIndex(currentIndex + 1));
-            // console.log(currentIndex);
-        }
+        const temp = (currentIndex + 1) >= imageCount ? 0 : currentIndex + 1;
+        setCurrentIndex(temp);
+        setImage(getImageByIndex(temp));
+
     }
 
     function decrease() {
-        if (currentIndex > 0) {
-            setCurrentIndex(currentIndex - 1);
-            setImage(getImageByIndex(currentIndex - 1));
-        }
+        const temp = (currentIndex - 1) < 0 ? 0 : currentIndex - 1;
+        setCurrentIndex(temp);
+        setImage(getImageByIndex(temp));
     }
-
-    
-    setTimeout(increase, 5000)
-    if(currentIndex == 5) {
-        setCurrentIndex(-1);
-    }
-
-    const text = 'فروشگاه آنلاین';
-    function writeText() {
-        setLetters(text.slice(0, index))
-        setIndex(index + 1)
-        if (index == text.length) {
-            setIndex(0)
-        }
-    }
-     setTimeout(writeText, 200);
 
     return (
         <div className="carousel-container" style={{ backgroundImage: `url(${img})` }}>
-            <i style={currentIndex == 0 ? { opacity: '0' } : { opacity: '1' }} className="material-icons left-icon" onClick={decrease}  >arrow_back_ios</i>
-            <div className='carousel-text' style={currentIndex==1?{textShadow: '1px 1px 4px black'}:{textShadow: 'rgb(129, 129, 129) 1px 1px 4px'}}>{letters}</div>
-            <i style={currentIndex == 5 ? { opacity: '0' } : { opacity: '1' }} className="material-icons right-icon" onClick={increase} >arrow_forward_ios</i>
+
+            <i style={currentIndex == 0 ? { visibility: 'hidden' } : { visibility: 'visible' }} className="material-icons left-icon" onClick={decrease}  >arrow_back_ios</i>
+            <div className='carousel-text' ><WriteText /></div>
+
+            <i style={currentIndex == 5 ? { visibility: 'hidden' } : { visibility: 'visible' }} className="material-icons right-icon" onClick={increase} >arrow_forward_ios</i>
+
         </div>
     )
 }
