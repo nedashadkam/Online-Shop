5824a096fb07908b71e04b400b58d88aa054607d
neda <neda.shadkam2003@gmail.com>
2024-08-03 17:32:13 +0330

    change in backdrop and hover mode of buttons and icons

--- src/components/RouteMarker/RouteMarker.js
+++ src/components/RouteMarker/RouteMarker.js
@@ -8,7 +8,7 @@ const RouteMarker = (props) => {
     return (
         <div className="route-marker">
             <MenuItems>
-                <MenuItem link='/store' style={{ color: 'rgb(155 155 155)', fontWeight: '100', marginLeft: '0', paddingLeft: '5px' }} >فروشگاه </MenuItem>
+                <MenuItem link='/store' style={{border: 'none', color: 'rgb(155 155 155)', fontWeight: '100', marginLeft: '0', paddingLeft: '5px' }} >فروشگاه </MenuItem>
                 <span style={{ color: 'rgb(155 155 155)' }}>/ {props.pageName}</span>
             </MenuItems>
         </div>
--- src/pages/Store/Store.js
+++ src/pages/Store/Store.js
@@ -8,26 +8,27 @@ import BackToTop from "../../components/BackToTop/BackToTop";
 
 const Store = () => {
     const menuItemStyle = {
-        border: '3px solid #445778',
-        padding: '30px',
-        borderRadius: '20px',
-        margin: '10px',
-        backgroundColor: 'rgb(248, 248, 248)',
-        boxSizing: 'border-box',
-        boxShadow: '0px 0px 4px darkgray'
-
-    }
+        // border: '3px solid #445778',
+        // padding: '30px',
+        // borderRadius: '20px',
+        // margin: '10px',
+        // backgroundColor: 'rgb(248, 248, 248)',
+        // boxSizing: 'border-box',
+        // boxShadow: '0px 0px 4px darkgray'
+        width: '72px', padding: '40px 10px', margin: '0',
+        }
+    
     return (
         <>
             <main className="store-container">
                 <div className="pages-path">
                     <MenuItems>
-                        <span><MenuItem style={{ width: '72px', padding: '40px 10px' }} link='/store/pottery'>   سفال</MenuItem></span>
-                        <span><MenuItem style={{ width: '72px', padding: '40px 10px' }} link='/store/wodden'> چوبی</MenuItem></span>
-                        <span><MenuItem style={{ width: '72px', padding: '40px 10px' }} link='/store/glass' > شیشه ای</MenuItem></span>
-                        <span><MenuItem style={{ width: '72px', padding: '40px 10px' }} link='/store/tablecloth'  >  رومیزی </MenuItem></span>
-                        <span><MenuItem style={{ width: '72px', padding: '40px 10px' }} link='/store/ceramic' >  سرامیک </MenuItem></span>
-                        <span><MenuItem style={{ width: '72px', padding: '40px 10px' }} link='/store/candle'>  شمع </MenuItem></span>
+                        <span><MenuItem style={menuItemStyle} link='/store/pottery'>   سفال</MenuItem></span>
+                        <span><MenuItem style={menuItemStyle} link='/store/wodden'> چوبی</MenuItem></span>
+                        <span><MenuItem style={menuItemStyle} link='/store/glass' > شیشه ای</MenuItem></span>
+                        <span><MenuItem style={menuItemStyle} link='/store/tablecloth'  >  رومیزی </MenuItem></span>
+                        <span><MenuItem style={menuItemStyle} link='/store/ceramic' >  سرامیک </MenuItem></span>
+                        <span><MenuItem style={menuItemStyle} link='/store/candle'>  شمع </MenuItem></span>
                     </MenuItems>
                 </div>
                 <Suggested />
